2d622566403eeabaab29c877948ab9e02d268a87
some-earth11 <some_earth@outlook.com>
2022-02-26 15:40:14 +0530

    Created Flask Temp

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from pricefetch import yup
-
 from flask import Flask, render_template, request, redirect
 
-print(yup('BTC'))
-print(yup('ETH'))
-print(yup('SOL'))
-print(yup('LTC'))
-print(yup('DOGE'))
-print(yup('ADA'))
-print(yup('SHIB'))
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("HW")
+
+if __name__== "__main__":
+    app.run(debug=True, port=8000)
+# print(yup('BTC'))
+# print(yup('ETH'))
+# print(yup('SOL'))
+# print(yup('LTC'))
+# print(yup('DOGE'))
+# print(yup('ADA'))
+# print(yup('SHIB'))
 
