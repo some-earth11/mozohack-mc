e4b8bc4d615f980e41b9e528f25aa878cfd65cd4
some-earth11 <some_earth@outlook.com>
2022-02-26 18:19:07 +0530

    Form INput

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def home():
-    return render_template('crypto.html')
+    if request.method=="POST":
+        amountbtc = request.form['amountbtc']
+        amountbtc = float(amountbtc)*yup('BTC')
+        amountbtc = amountbtc*0.3
+
+    return render_template('crypto.html',taxed_btc = amountbtc)
 
 if __name__== "__main__":
     app.run(debug=True, port=8000)
