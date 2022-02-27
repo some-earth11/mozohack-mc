a5a13f0286a782d8d4a4e3505c5715cb43097efe
some-earth11 <some_earth@outlook.com>
2022-02-28 01:43:17 +0530

    Bitcoin Working

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from pricefetch import yup
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def home():
-    if request.method=="POST":
-        amountbtc = request.form['amountbtc']
-        amountbtc = float(amountbtc)*yup('BTC')
-        amountbtc = amountbtc*0.3
+    if request.method == "POST":
+        btc = request.form["amountbtc"]
+        tbtc = float(btc)*yup('BTC')*0.3
 
-    return render_template('crypto.html',taxed_btc = amountbtc)
+        return redirect(url_for("user",usr=tbtc))
+    else:
+        return render_template("crypto.html")
+
+
+@app.route("/<usr>")
+def user(usr):
+    return render_template("crypto.html",usr=usr)
 
 if __name__== "__main__":
     app.run(debug=True, port=8000)
